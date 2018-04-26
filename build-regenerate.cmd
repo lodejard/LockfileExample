bb2ef8bcea8de1a125d2f644a924aa153225758d
Louis DeJardin <lodejard@microsoft.com>
2018-04-25 23:09:04 -0700

    Updating message and cmd file

--- build-regenerate.cmd
+++ build-regenerate.cmd
@@ -1,10 +1,7 @@
 setlocal
 del project.assets.json /s
 set ApplyLockFile=true
-set FlattenLockFile=false
+set VerifyLockFile=false
 set GenerateLockFile=true
 call build
 
-set FlattenLockFile=true
-call build
-call build
